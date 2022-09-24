4e4caf606a070beb2b69afb8d6cff04063e95561
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-09-24 01:05:43 +0100

    Update script.js

--- M/html/script.js
+++ M/html/script.js
@@ -1,167 +1,203 @@
-'use strict';
-const openModal = document.querySelector('.support-btn');
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.close-modal');
-const overlay = document.querySelector('.overlay');
-const characterContainer = document.querySelector('.chracters--list');
-const statusType = document.querySelector('.status-type');
+"use strict";
+
+const mainSection = document.querySelector(".chracters__section");
+const spinner = document.querySelector(".spinner");
+const errorHandler = document.querySelector(".errorHander");
+const defaultPage = document.querySelector(".default__page");
+const searchSection = document.querySelector(".search__section");
 const searchBar = document.querySelector(".search");
 const searchInput = document.querySelector(".search__field");
-const block = document.querySelector(".block");
-const searchSection = document.querySelector(".search__section");
-// let green = document.querySelector('.status-type').style.backgroundColor = 'yellow';
-// let red = document.querySelector('.status-type').style.backgroundColor = 'pink';
-const errorHandler = document.querySelector(".error");
-const showError = (errorHandler.visibility = "visible");
+const searchBtn = document.querySelector(".search__btn");
+const searchCloseBtn = document.querySelector(".search--close--btn");
 
-const showModal = function() {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
+const renderSpinner = () => {
+    const markup = `
+        <svg>
+            <use href="img/icons.svg#icon-loader"></use>
+        </svg>
+`;
+    spinner.insertAdjacentHTML("beforeend", markup);
 };
 
-const removeModal = function() {
-    modal.classList.add("hidden");
-    overlay.classList.add("hidden");
-};
+renderSpinner();
 
-openModal.addEventListener("click", showModal);
+let defaultError =
+    "Took to long to load check your connection and refresh this page";
 
-closeModal.addEventListener("click", removeModal);
-
-document.addEventListener("keydown", function(e) {
-    if (e.key === "Escape") {
-        modal.classList.add("hidden");
-        overlay.classList.add("hidden");
-    }
-});
-
-overlay.addEventListener("click", removeModal);
-
-const createChracter = function(data, data2) {
-    const html = `
-           <div class="chracter">
-                <div>
-                    <img src="${data.image}" alt="">
-                </div>
-                <div class="chracter__info">
-                    <h2>${data.name}</h2>
-                    <div class="check-status">
-                        <b class="status-type">${
-                          data.status === "Alive" ? "yes" : "no"
-                        }</b>
-                       <h4>${data.status} - ${data.species}</h4>
-                    </div>
-                    <span>Last known location:</span>
-                    <p class="location"><a href="${data.location.url}">${
-    data.location.name
-  }</a></p>
-                    <span>First seen in:</span>
-                    <p style="margin-top:.6em;"><a href='${data.episode[0]}'>${
-    data2.name
-  }</a></p>
-                </div>
-            </div>
+const errorMessage = () => {
+    const message = `
+    <div class="error">
+        <div>
+            <svg>
+            <use href="img/icons.svg#icon-alert-triangle"></use>
+          </svg>
+        </div>
+        <p>${defaultError}</p>
+    </div>
     `;
-    block.insertAdjacentHTML("beforeend", html);
-};
-//<b class="status-type">.${data.status === "Alive" ? green : red}</b>
-//<p style="margin-top:.6em;"><a href='#'>${data2.name}</a></p>
-// <p style="margin-top:.6em;"><a href='#'>${data.episode[0]}</a></p>
-
-const getChracter = async function(character, episode) {
-    setTimeout(function() {
-        try {
-            fetch(`https://rickandmortyapi.com/api/character/${character}`)
-                .then((res) => {
-                    return res.json();
-                })
-                .then((info1) => {
-                    console.log(info1);
-                    //createChracter(info1)
-                    fetch(`https://rickandmortyapi.com/api/episode/${episode}`)
-                        .then((res2) => {
-                            return res2.json();
-                        })
-                        .then((info2) => {
-                            console.log(info2);
-                            createChracter(info1, info2);
-                        });
-                });
-            //if (!res.ok) throw new Error(`${showError} (${info1.status})`)
-        } catch (error) {
-            showError.style.visibility = "visible";
-        }
-    }, 5000);
+    errorHandler.insertAdjacentHTML("beforeend", message);
 };
 
+const green = `<b style="background-color: #55cc44; color:#55cc44;">.</b>`;
+const red = `<b style="background-color: #d63d2e; color: #d63d2e;">.</b>`;
+
+const createChracter = (data, data2) => {
+        const html = `
+    <div class="chracter">
+    <div>
+        <img src="${data.image}" alt="">
+    </div>
+    <div class="chracter__info">
+        <h2>${data.name}</h2>
+        <div class="check-status">
+        ${data.status === "Alive" ? `${green}` : `${red}`}
+            <h4>${data.status} - ${data.species}</h4>
+        </div>
+        <div class="location__info">
+            <span>Last known location :</span>
+            <p><a href="${data.location.url}">${data.location.name}</a></p>
+        </div>
+        <br>
+        <div class="location__info">
+            <span>First seen in :</span>
+            <p>
+                <a href='${data.episode[0]}'>${data2.name}</a>
+            </p>
+        </div>
+    </div>
+    `;
+  defaultPage.insertAdjacentHTML("beforeend", html);
+};
+
+const searchResult = (data) => {
+  const html = `
+        <div class="chracter seee">
+            <div>
+                <img src="https://rickandmortyapi.com/api/character/avatar/${
+                  data.id
+                }.jpeg" alt="">
+            </div>
+        
+            <div class="chracter__info">
+            <h2>${data.name}</h2>
+            <div class="check-status">
+            ${data.status === "Alive" ? `${green}` : `${red}`}
+                <h4>${data.status}</h4>
+            </div>
+            <div class="location__info">
+                <span>Species :</span>
+                <p class="info-p">${data.species}</p>
+            </div>
+            <br>
+            <div class="location__info">
+                <span>Type :</span>
+                <p class="info-p">${
+                  data.type === "" ? "unknown" : data.type
+                }</p>
+            </div>
+            </div>
+        </div>
+    `;
+  searchSection.insertAdjacentHTML("beforeend", html);
+};
+
+const getChracter = async (character, episode) => {
+  setTimeout(() => {
+    try {
+      fetch(`https://rickandmortyapi.com/api/character/${character}`)
+        .then((res) => {
+          //console.log(res);
+          return res.json();
+        })
+        .then((info1) => {
+          //console.log(info1);
+          fetch(`https://rickandmortyapi.com/api/episode/${episode}`)
+            .then((res) => {
+              return res.json();
+            })
+            .then((info2) => {
+              console.log(info2);
+              defaultPage.classList.remove("hidden");
+              createChracter(info1, info2);
+              spinner.classList.add("hidden");
+            });
+        });
+    } catch (error) {
+      //alert(error);
+      errorMessage();
+    }
+  }, 3000);
+};
 getChracter(30, 31);
 getChracter(215, 10);
 getChracter(31, 15);
 getChracter(225, 19);
-getChracter(256, 21);
+getChracter(453, 18);
 getChracter(350, 30);
 getChracter(814, 51);
-getChracter(800, 47);
+getChracter(578, 36);
 getChracter(230, 7);
 getChracter(120, 5);
 
-searchBar.addEventListener("submit", function(e) {
-    e.preventDefault();
-    const query = searchInput.value;
-    block.style.display = "none";
-    const loadSearchResults = function(query) {
-        setTimeout(function() {
-            try {
-                fetch(`https://rickandmortyapi.com/api/character/?name=${query}`)
-                    .then((res) => {
-                        return res.json();
-                    })
-                    .then((data) => {
-                        console.log(data);
-                        const search = data.results;
-                        console.log(search);
-                        search.forEach((rec) => {
-                            const html = `
-                        <div class="chracter">
-                        <div>
-                            <img src="https://rickandmortyapi.com/api/character/avatar/${
-                              rec.id
-                            }.jpeg" alt="">
-                        </div>
-        
-                        <div class="chracter__info">
-                            <h2 style="margin-bottom: .3em;">${rec.name}</h2>
-                            <span>status:</span>
-                            <div class="check-status">
-                                <b class="status-type">
-                                ${rec.status === "Alive" ? "yes" : "no"}
-                                </b>
-                                <h4 style="margin-bottom: .4em;">${
-                                  rec.status
-                                }</h4>
-                            </div>
-                            <span>Species:</span>
-                            <br>
-                            <p style="margin-top: .5em; margin-bottom: .5em;color:white;">${
-                              rec.species
-                            }</p>
-                            <span>Type:</span>
-                            <p style="margin-top:.5em;color:white;">${
-                              rec.type === "" ? "unknown" : rec.type
-                            }</p>
-                        </div>
-                    </div>`;
-                            //console.log(html);
-                            document
-                                .querySelector(".search__section")
-                                .insertAdjacentHTML("beforeend", html);
-                        });
-                    });
-                searchInput.value = "";
-            } catch (err) {
-                alert(`${err}`);
-            }
-        }, 1000);
-    };
+searchBar.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  defaultPage.style.display = "none";
+  spinner.classList.remove("hidden");
+  const query = searchInput.value;
+  const loadSearchResults = (query) => {
+    setTimeout(() => {
+      try {
+        fetch(`https://rickandmortyapi.com/api/character/?name=${query}`)
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            //console.log(data);
+            spinner.classList.add("hidden");
+            //errorHandler.style.display = 'none';
+            const result = data.results;
+            result.map((rec) => {
+              searchResult(rec);
+            });
+          });
+        searchBtn.disabled = true;
+      } catch (error) {
+        console.log(`no result`);
+        errorMessage();
+      }
+    }, 3000);
+  };
+  if (searchInput.value === "") {
+    searchSection.style.display = "none";
+    defaultError = "Search input is empty ; please search for a chracters name";
+    errorMessage();
+    searchBtn.disabled = true;
+  } else {
     loadSearchResults(query);
+  }
+
+  loadSearchResults(query);
 });
+
+searchInput.addEventListener("click", () => {
+  searchCloseBtn.classList.remove("hidden");
+});
+
+searchCloseBtn.addEventListener("click", () => {
+  searchCloseBtn.classList.add("hidden");
+  searchInput.value = "";
+  //searchSection.style.display = 'none';
+  searchSection.innerHTML = "";
+  defaultPage.style.display = "block";
+  errorHandler.style.display = "none";
+  searchBtn.disabled = false;
+});
+// const Timeout = function(s) {
+//     return new Promise((_, reject) => {
+//         setTimeout(() => {
+//             reject(console.log(`Took to long to load try again`));
+//         }, s * 3000);
+//     });
+// };
+// Timeout();
