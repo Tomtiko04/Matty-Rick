64174db34ec5007b57fdf241ba297ca58391ca93
Ogunneye <83877877+Tomtiko04@users.noreply.github.com>
2022-10-01 20:33:56 +0100

    Update script.js

--- M/html/script.js
+++ M/html/script.js
@@ -1,14 +1,21 @@
-"use strict";
+'use strict';
 
-const mainSection = document.querySelector(".chracters__section");
-const spinner = document.querySelector(".spinner");
-const errorHandler = document.querySelector(".errorHander");
-const defaultPage = document.querySelector(".default__page");
-const searchSection = document.querySelector(".search__section");
-const searchBar = document.querySelector(".search");
-const searchInput = document.querySelector(".search__field");
-const searchBtn = document.querySelector(".search__btn");
-const searchCloseBtn = document.querySelector(".search--close--btn");
+const mainSection = document.querySelector('.chracters__section');
+const spinner = document.querySelector('.spinner');
+const errorHandler = document.querySelector('.errorHander');
+const defaultPage = document.querySelector('.default__page');
+const searchSection = document.querySelector('.search__section');
+const searchBar = document.querySelector('.search');
+const searchInput = document.querySelector('.search__field');
+const searchBtn = document.querySelector('.search__btn');
+const searchCloseBtn = document.querySelector('.search--close--btn');
+const readMoreControl = document.querySelector('.read__more');
+
+const green = `<b style="background-color: #55cc44; color:#55cc44;" class="biggg">.</b>`;
+const red = `<b style="background-color: #d63d2e; color: #d63d2e;" class="biggg">.</b>`;
+let defaultError =
+    'Took to long to load check your connection and refresh this page';
+let page = 1;
 
 const renderSpinner = () => {
     const markup = `
@@ -16,13 +23,12 @@ const renderSpinner = () => {
             <use href="img/icons.svg#icon-loader"></use>
         </svg>
 `;
-    spinner.insertAdjacentHTML("beforeend", markup);
+    spinner.insertAdjacentHTML('beforeend', markup);
 };
 
 renderSpinner();
 
-let defaultError =
-    "Took to long to load check your connection and refresh this page";
+
 
 const errorMessage = () => {
     const message = `
@@ -35,13 +41,12 @@ const errorMessage = () => {
         <p>${defaultError}</p>
     </div>
     `;
-    errorHandler.insertAdjacentHTML("beforeend", message);
+    errorHandler.insertAdjacentHTML('beforeend', message);
 };
 
-const green = `<b style="background-color: #55cc44; color:#55cc44;">.</b>`;
-const red = `<b style="background-color: #d63d2e; color: #d63d2e;">.</b>`;
 
-const createChracter = (data, data2) => {
+
+const createChracter = data => {
         const html = `
     <div class="chracter">
     <div>
@@ -50,7 +55,7 @@ const createChracter = (data, data2) => {
     <div class="chracter__info">
         <h2>${data.name}</h2>
         <div class="check-status">
-        ${data.status === "Alive" ? `${green}` : `${red}`}
+        ${data.status === 'Alive' ? `${green}` : `${red}`}
             <h4>${data.status} - ${data.species}</h4>
         </div>
         <div class="location__info">
@@ -59,17 +64,15 @@ const createChracter = (data, data2) => {
         </div>
         <br>
         <div class="location__info">
-            <span>First seen in :</span>
-            <p>
-                <a href='${data.episode[0]}'>${data2.name}</a>
-            </p>
+          <span>Type :</span>
+            <p class="info-p">${data.type === '' ? 'unknown' : data.type}</p>
         </div>
     </div>
     `;
-  defaultPage.insertAdjacentHTML("beforeend", html);
+  defaultPage.insertAdjacentHTML('beforeend', html);
 };
 
-const searchResult = (data) => {
+const searchResult = data => {
   const html = `
         <div class="chracter seee">
             <div>
@@ -81,7 +84,7 @@ const searchResult = (data) => {
             <div class="chracter__info">
             <h2>${data.name}</h2>
             <div class="check-status">
-            ${data.status === "Alive" ? `${green}` : `${red}`}
+            ${data.status === 'Alive' ? `${green}` : `${red}`}
                 <h4>${data.status}</h4>
             </div>
             <div class="location__info">
@@ -92,112 +95,127 @@ const searchResult = (data) => {
             <div class="location__info">
                 <span>Type :</span>
                 <p class="info-p">${
-                  data.type === "" ? "unknown" : data.type
+                  data.type === '' ? 'unknown' : data.type
                 }</p>
             </div>
             </div>
         </div>
     `;
-  searchSection.insertAdjacentHTML("beforeend", html);
+  searchSection.insertAdjacentHTML('beforeend', html);
 };
 
-const getChracter = async (character, episode) => {
+const readMoreCreate = () => {
+  const html = `
+  <button>Read more</button>
+  `;
+  readMoreControl.insertAdjacentHTML('beforeend', html);
+};
+
+
+const getChracter = async () => {
   setTimeout(() => {
     try {
-      fetch(`https://rickandmortyapi.com/api/character/${character}`)
-        .then((res) => {
-          //console.log(res);
+      fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+        .then(res => {
+          console.log(res);
           return res.json();
         })
-        .then((info1) => {
-          //console.log(info1);
-          fetch(`https://rickandmortyapi.com/api/episode/${episode}`)
-            .then((res) => {
-              return res.json();
-            })
-            .then((info2) => {
-              console.log(info2);
-              defaultPage.classList.remove("hidden");
-              createChracter(info1, info2);
-              spinner.classList.add("hidden");
-            });
+        .then(res => {
+          const result1 = res.results;
+          result1.map(data => {
+            createChracter(data);
+            spinner.classList.add('hidden');
+          });
         });
     } catch (error) {
-      //alert(error);
+      console.log(error);
       errorMessage();
     }
-  }, 3000);
+  }, 4000);
 };
-getChracter(30, 31);
-getChracter(215, 10);
-getChracter(31, 15);
-getChracter(225, 19);
-getChracter(453, 18);
-getChracter(350, 30);
-getChracter(814, 51);
-getChracter(578, 36);
-getChracter(230, 7);
-getChracter(120, 5);
 
-searchBar.addEventListener("submit", function (e) {
+const readMore = () => {
+  readMoreCreate();
+  readMoreControl.addEventListener('click', function (e) {
+    spinner.classList.add('hidden');
+    const btn = e.target.closest('button');
+    if (!btn) return;
+    page++;
+    setTimeout(() => {
+      try {
+        fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
+          .then(res => {
+            console.log(res);
+            return res.json();
+          })
+          .then(inf => {
+            console.log(inf);
+            const result1 = inf.results;
+            console.log(result1);
+            result1.map(data => {
+              createChracter(data);
+            });
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }, 500);
+  });
+};
+getChracter();
+readMore();
+
+searchBar.addEventListener('submit', function (e) {
   e.preventDefault();
-
-  defaultPage.style.display = "none";
-  spinner.classList.remove("hidden");
+  readMoreControl.classList.add('hidden');
+  defaultPage.style.display = 'none';
+  spinner.classList.remove('hidden');
+  searchBtn.disabled = true;
+  let result = [];
+  searchSection.innerHTML = '';
   const query = searchInput.value;
-  const loadSearchResults = (query) => {
+  const loadSearchResults = query => {
     setTimeout(() => {
       try {
         fetch(`https://rickandmortyapi.com/api/character/?name=${query}`)
-          .then((res) => {
+          .then(res => {
             return res.json();
           })
-          .then((data) => {
-            //console.log(data);
-            spinner.classList.add("hidden");
-            //errorHandler.style.display = 'none';
-            const result = data.results;
-            result.map((rec) => {
+          .then(data => {
+            spinner.classList.add('hidden');
+            searchBtn.disabled = false;
+            result = data.results;
+            result.map(rec => {
               searchResult(rec);
             });
           });
-        searchBtn.disabled = true;
       } catch (error) {
-        console.log(`no result`);
         errorMessage();
       }
-    }, 3000);
+    }, 1000);
   };
-  if (searchInput.value === "") {
-    searchSection.style.display = "none";
-    defaultError = "Search input is empty ; please search for a chracters name";
-    errorMessage();
+
+  if (searchInput.value === '') {
     searchBtn.disabled = true;
+    defaultError = 'Search input is empty ; please search for a chracters name';
+    errorMessage();
+    searchBtn.disabled = false;
   } else {
+    errorHandler.style.display = 'none';
     loadSearchResults(query);
   }
-
-  loadSearchResults(query);
 });
 
-searchInput.addEventListener("click", () => {
-  searchCloseBtn.classList.remove("hidden");
+searchInput.addEventListener('click', () => {
+  searchCloseBtn.classList.remove('hidden');
 });
 
-searchCloseBtn.addEventListener("click", () => {
-  searchCloseBtn.classList.add("hidden");
-  searchInput.value = "";
-  //searchSection.style.display = 'none';
-  searchSection.innerHTML = "";
-  defaultPage.style.display = "block";
-  errorHandler.style.display = "none";
+searchCloseBtn.addEventListener('click', () => {
+  searchCloseBtn.classList.add('hidden');
+  searchInput.value = '';
+  searchSection.innerHTML = '';
+  defaultPage.style.display = 'grid';
+  errorHandler.style.display = 'none';
   searchBtn.disabled = false;
+  readMoreControl.classList.remove('hidden');
 });
-// const Timeout = function(s) {
-//     return new Promise((_, reject) => {
-//         setTimeout(() => {
-//             reject(console.log(`Took to long to load try again`));
-//         }, s * 3000);
-//     });
-// };
-// Timeout();
